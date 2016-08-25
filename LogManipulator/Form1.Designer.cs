267f1c1b9874d628962c284f5ca4422b4d08ab77
ahornsey <saab99e@googlemail.com>
2016-08-25 12:36:40 +0800

    Upload code to dir

---
+++ LogManipulator/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace LogManipulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxAdjustment = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxFileType = new System.Windows.Forms.TextBox();
+            this.listHeadings = new System.Windows.Forms.ListBox();
+            this.textItemNo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelMaxItems = new System.Windows.Forms.Label();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select File...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(184, 82);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(533, 22);
+            this.textBoxFileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Filename";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Headings";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(187, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Adjustment";
+            // 
+            // textBoxAdjustment
+            // 
+            this.textBoxAdjustment.Location = new System.Drawing.Point(280, 247);
+            this.textBoxAdjustment.Name = "textBoxAdjustment";
+            this.textBoxAdjustment.Size = new System.Drawing.Size(71, 22);
+            this.textBoxAdjustment.TabIndex = 11;
+            this.textBoxAdjustment.Text = "0";
+            this.textBoxAdjustment.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "File Type";
+            // 
+            // textBoxFileType
+            // 
+            this.textBoxFileType.Location = new System.Drawing.Point(184, 110);
+            this.textBoxFileType.Name = "textBoxFileType";
+            this.textBoxFileType.Size = new System.Drawing.Size(51, 22);
+            this.textBoxFileType.TabIndex = 14;
+            this.textBoxFileType.Text = "0";
+            this.textBoxFileType.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // listHeadings
+            // 
+            this.listHeadings.FormattingEnabled = true;
+            this.listHeadings.ItemHeight = 16;
+            this.listHeadings.Location = new System.Drawing.Point(184, 138);
+            this.listHeadings.Name = "listHeadings";
+            this.listHeadings.Size = new System.Drawing.Size(533, 100);
+            this.listHeadings.TabIndex = 17;
+            this.listHeadings.SelectedIndexChanged += new System.EventHandler(this.listHeadings_SelectedIndexChanged);
+            // 
+            // textItemNo
+            // 
+            this.textItemNo.Location = new System.Drawing.Point(613, 244);
+            this.textItemNo.Name = "textItemNo";
+            this.textItemNo.Size = new System.Drawing.Size(51, 22);
+            this.textItemNo.TabIndex = 19;
+            this.textItemNo.Text = "0";
+            this.textItemNo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textItemNo.TextChanged += new System.EventHandler(this.textItemNo_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(502, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Selected Item #";
+            // 
+            // labelMaxItems
+            // 
+            this.labelMaxItems.AutoSize = true;
+            this.labelMaxItems.Location = new System.Drawing.Point(670, 247);
+            this.labelMaxItems.Name = "labelMaxItems";
+            this.labelMaxItems.Size = new System.Drawing.Size(32, 17);
+            this.labelMaxItems.TabIndex = 20;
+            this.labelMaxItems.Text = "of 0";
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Location = new System.Drawing.Point(61, 294);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(155, 30);
+            this.buttonProcess.TabIndex = 21;
+            this.buttonProcess.Text = "Process File";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 348);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.labelMaxItems);
+            this.Controls.Add(this.textItemNo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listHeadings);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxFileType);
+            this.Controls.Add(this.textBoxAdjustment);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SkyEye File Details";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxAdjustment;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxFileType;
+        private System.Windows.Forms.ListBox listHeadings;
+        private System.Windows.Forms.TextBox textItemNo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelMaxItems;
+        private System.Windows.Forms.Button buttonProcess;
+    }
+}
+
---
+++ LogManipulator/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+
+
+
+
+namespace LogManipulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                /*openFileDialog1.FileName);*/
+                string FileName = openFileDialog1.FileName;
+                textBoxFileName.Text = FileName;
+                listHeadings.Items.Clear();
+                updateFileDetails(sender, e);
+
+            }
+        }
+
+        private void updateFileDetails(object sender, EventArgs e)
+        {
+
+            string FileName = textBoxFileName.Text;
+
+            if (FileName == "")
+            {
+                return;
+            }
+
+            using (ShellObject shell = ShellObject.FromParsingName(FileName))
+            {
+                // alternatively: shell.Properties.GetProperty("System.Media.Duration");
+                IShellProperty prop = shell.Properties.System.Media.Duration;
+                // Duration will be formatted as 00:44:08
+                //string duration = prop.FormatForDisplay(PropertyDescriptionFormatOptions.None);
+                //string[] hms = duration.Split(':');
+
+                string filetype = shell.Properties.System.ItemType.Value;
+                textBoxFileType.Text = filetype;
+
+                //Parse the headers and populate the listbox
+                read_TSVFile(sender, e, textBoxFileName.Text, true);
+            }
+
+        }
+
+        private void read_TSVFile(object sender, EventArgs e, string FileName, bool HeadersOnly)
+        {
+       
+            string line;
+            string regexString = "";
+            string regexStringFooter = "";
+            string splitChar = "";
+            string HeaderFile = "";
+            string ProcessedFile = "";
+
+            if (HeadersOnly)
+            {
+                labelMaxItems.Text = " of 0";
+                listHeadings.ResetText();
+                ProcessedFile = FileName + ".headers" + textBoxFileType.Text;
+
+            }
+            else
+            {
+                HeaderFile = FileName + ".headers" + textBoxFileType.Text;
+                ProcessedFile = FileName + ".processed" + textBoxFileType.Text;
+            }
+
+            int fieldToMod = Int16.Parse(textItemNo.Text);
+
+            if (FileName == "")
+            {
+                return; 
+            }
+
+            // Read the file and display it line by line.
+
+            StreamReader file = new StreamReader(FileName);
+
+            StreamWriter outputFile = new StreamWriter(ProcessedFile);
+
+            if (textBoxFileType.Text == ".csv")
+            {
+                regexString = @"^CUSTOM.updateTime";
+                //This is a fudge there is no footer in the csv file;
+                regexStringFooter = @"#Flight Recorder Session End";
+                splitChar = ",";
+            }
+            else if (textBoxFileType.Text == ".tsv")
+            {
+                regexString = @"^Date\/Time \(GMT\)";
+                regexStringFooter = @"#Flight Recorder Session End";
+                splitChar = "\t";
+            }
+            else
+            {
+                MessageBox.Show ("Could not Recognise File Type","File Not Recognised");
+            }
+           
+            bool isHeader = true;
+            bool isFooter = false;
+            bool Continue = true;
+            bool Success = true;
+
+            string strHeader = "";
+            string[] strHeadings = new string[0];
+
+            while ( Success && Continue && (line = file.ReadLine()) != null )
+            {
+                //Console.WriteLine(line);
+
+                Regex regexFooter = new Regex(regexStringFooter);
+                Match matchFooter = regexFooter.Match(line);
+
+                if (matchFooter.Success)
+                {
+                    isFooter = true;
+                }
+
+                if (isHeader)
+                {
+                    Regex regexHeader = new Regex(regexString);
+                    Match match = regexHeader.Match(line);
+                    if (match.Success)
+                    {
+                        isHeader = false;
+                        if (HeadersOnly)
+                        {
+                            strHeadings = line.Split(splitChar.ToCharArray());
+                            listHeadings.Items.AddRange(strHeadings);
+                            int itemcount = listHeadings.Items.Count - 1;
+                            labelMaxItems.Text = "of " + itemcount.ToString();
+                            Continue = false;
+                        }
+                        else
+                        {
+                            outputFile.WriteLine(line);
+                        }
+                    }
+                    else
+                    {
+                        strHeader = strHeader + line;
+                        if (HeadersOnly)
+                        {
+                            outputFile.WriteLine(line);
+                        }
+                    }
+                }
+                else if (isFooter)
+                {
+
+                    //StreamWriter outputFooter = new StreamWriter(HeaderFile, true); }
+                    //outputFooter.WriteLine(line);
+
+                }
+                else if (!isHeader)
+                {
+                    // Manipulate the data here
+                    //outputFile.WriteLine(line);
+                    var values = line.Split(splitChar.ToCharArray());
+                    string strModified = "";
+                    string strModField = values[fieldToMod].ToString();
+                    double dblmodField = 0;
+                    if (strModField != "")
+                    {
+
+                        try
+                        {
+                            dblmodField = Double.Parse(strModField);
+
+                        }
+                        catch (FormatException fe)
+                        {
+                            strModified = strModField;
+                            Success = false;
+                            string Message = "Failed to parse field '" + fieldToMod.ToString() + "' Value '" + strModField + "'\nError Message '" + fe.Message + "'";
+                            MessageBox.Show(Message, "Failed to Parse Field");
+
+                        }
+
+                    }
+                    else
+                    {
+                        dblmodField = 0;
+                    }
+
+
+                    double modValue = 0;
+                    try
+                    {
+                        modValue = Double.Parse(textBoxAdjustment.Text);
+                    }
+                    catch (FormatException fe)
+                    {
+                        strModified = strModField;
+                        Success = false;
+                        string Message = "Failed to parse Adjustment Value '" + textBoxAdjustment.Text + "'\nError Message '" + fe.Message +"'";
+                        MessageBox.Show(Message, "Failed to Parse Adjustment Value");
+
+                    }
+                    
+
+                    dblmodField = dblmodField + modValue;
+                    strModified = dblmodField.ToString();
+                    values[fieldToMod] = dblmodField.ToString();
+                    outputFile.WriteLine( String.Join(splitChar, values));
+                    //outputFile.WriteLine("Field to Mod " + fieldToMod.ToString() + " Value " + strModField + " Modified:" + strModified);
+                }
+  
+            }
+
+            file.Close();
+            outputFile.Close();
+
+            if (Success && !HeadersOnly)
+            {
+                MessageBox.Show("Operation Completed Successfully", "File Processing Complete");
+            } else if (!Success && HeadersOnly)
+            {
+                File.Delete(HeaderFile);
+            } else if (!Success)
+            {
+                File.Delete(HeaderFile);
+                File.Delete(ProcessedFile);
+            }
+
+            
+
+          
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            textBoxAdjustment.Text = Convert.ToString(Convert.ToInt16(textBoxAdjustment.Text) + 1);
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            textBoxAdjustment.Text = Convert.ToString(Convert.ToInt16(textBoxAdjustment.Text) - 1);
+        }
+
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            read_TSVFile(sender, e, textBoxFileName.Text , true);
+        }
+
+        private void listHeadings_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textItemNo.Text = listHeadings.SelectedIndex.ToString();
+        }
+
+        private void textItemNo_TextChanged(object sender, EventArgs e)
+        {
+            int indSelected = 0;
+
+            try
+            {
+                indSelected = Int16.Parse(textItemNo.Text);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                listHeadings.SelectedIndex = indSelected;
+            }
+            catch ( ArgumentOutOfRangeException ae)
+            {
+                Console.WriteLine(ae.Message);
+                listHeadings.SelectedIndex = 0;
+                //textItemNo.Text = "0";
+            }
+            
+        }
+
+        private void buttonProcess_Click_1(object sender, EventArgs e)
+        {
+            read_TSVFile(sender, e, textBoxFileName.Text, false);
+        }
+    }
+}
